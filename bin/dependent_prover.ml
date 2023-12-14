380fef6f3dc48f751ed3ea9354c7271226a9372b
Clément CHAPOT <clement@chapot.ovh>
2023-12-14 15:36:08 +0100

    add type inference (5.9 & 5.10) and fix misnamed alpha

--- bin/dependent_prover.ml
+++ bin/dependent_prover.ml
@@ -65,15 +65,9 @@ let rec string_of_context = function
       ^ (match t with Some t -> " = " ^ to_string t | _ -> "")
       ^ "\n" ^ string_of_context q
 
-let rec value_from_context v = function
-  | [] -> None
-  | (x, (_, Some t)) :: _ when x = v -> Some t
-  | (x, (_, None)) :: _ when x = v -> None
-  | (_, (_, _)) :: q -> value_from_context v q
-
 let rec red env = function
   | Type -> None
-  | Var v -> value_from_context v env
+  | Var v -> snd (List.assoc v env)
   | App (Abs (x, _, y), u) -> Some (subst x u y)
   | App (r, s) -> (
       let rr = red env r and ss = red env s in
@@ -97,26 +91,46 @@ let rec red env = function
 let rec normalize env t =
   match red env t with None -> t | Some s -> normalize env s
 
-let rec conv env e f =
+let rec alpha env e f =
   match (e, f) with
   | Type, Type -> true
   | Var v, Var w -> v = w
-  | App (f, g), App (f', g') -> conv env f f' && conv env g g'
+  | App (f, g), App (f', g') -> alpha env f f' && alpha env g g'
   | Abs (x, a, t), Abs (x', a', t') when x = x' ->
-      conv env a a' && conv env t t'
+      alpha env a a' && alpha env t t'
   | Abs (x, a, t), Abs (x', a', t') ->
       let y = fresh_var () in
-      conv env
+      alpha env
         (Abs (y, subst x (Var y) a, subst x (Var y) t))
         (Abs (y, subst x' (Var y) a', subst x' (Var y) t'))
-  | Pi (x, a, b), Pi (x', a', b') when x = x' -> conv env a a' && conv env b b'
+  | Pi (x, a, b), Pi (x', a', b') when x = x' ->
+      alpha env a a' && alpha env b b'
   | Pi (x, a, b), Pi (x', a', b') ->
       let y = fresh_var () in
-      conv env
+      alpha env
         (Pi (y, subst x (Var y) a, subst x (Var y) b))
         (Pi (y, subst x' (Var y) a', subst x' (Var y) b'))
   | _, _ -> false
 
 let conv env e f =
   let ne = normalize env e and nf = normalize env f in
-  conv env ne nf
+  alpha env ne nf
+
+exception Type_error
+exception Unbound_variable of var
+
+let rec infer env = function
+  | Type -> Type
+  | Var x -> (
+      try fst (List.assoc x env) with Not_found -> raise (Unbound_variable x))
+  | App (t, u) -> (
+      match infer env t with
+      | Pi (x, a, b) ->
+          if infer env u <> a then raise Type_error else subst x u b
+      | _ -> raise Type_error)
+  | Abs (x, a, t) ->
+      let b = infer ((x, (a, None)) :: env) t in
+      Pi (x, a, b)
+  | Pi (_, _, _) -> Type
+
+let check env t a = if infer env t <> a then raise Type_error else ()
